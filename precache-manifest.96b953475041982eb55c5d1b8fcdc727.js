8eeccede7f1f760edeec4228951884a664c87489
Adair Daniels <adairdaniels@gmail.com>
2020-03-22 23:26:03 -0700

    Updates

---
+++ precache-manifest.96b953475041982eb55c5d1b8fcdc727.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f013492bac38cc79941daebd87538a1e",
+    "url": "/platt-mockup/index.html"
+  },
+  {
+    "revision": "7dab627cd9376d175a68",
+    "url": "/platt-mockup/static/css/main.62a8d833.chunk.css"
+  },
+  {
+    "revision": "c724520c7990a293e11e",
+    "url": "/platt-mockup/static/js/2.4a4c4daa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/platt-mockup/static/js/2.4a4c4daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dab627cd9376d175a68",
+    "url": "/platt-mockup/static/js/main.96c1d9d0.chunk.js"
+  },
+  {
+    "revision": "4a29930c1e7a3220966f",
+    "url": "/platt-mockup/static/js/runtime-main.cd2238e7.js"
+  },
+  {
+    "revision": "4d61afda0e20f46bffcede4665e865e1",
+    "url": "/platt-mockup/static/media/11.4d61afda.png"
+  },
+  {
+    "revision": "f453e38b5f5c691c57b03368fc3f80c3",
+    "url": "/platt-mockup/static/media/app-get-banner.f453e38b.png"
+  },
+  {
+    "revision": "9546369d32279c61f26157a615aee7e5",
+    "url": "/platt-mockup/static/media/datacomm.9546369d.png"
+  },
+  {
+    "revision": "16f2f668ca6440dabf6fbf8417394628",
+    "url": "/platt-mockup/static/media/deal.16f2f668.png"
+  },
+  {
+    "revision": "827698441c5f8eb428c6e3f00487b2f2",
+    "url": "/platt-mockup/static/media/energy.82769844.png"
+  },
+  {
+    "revision": "330d646f75e0aea829e6e759048293cf",
+    "url": "/platt-mockup/static/media/hvac.330d646f.png"
+  },
+  {
+    "revision": "ff08a92f070ce30f0c6ffb57f97bd65f",
+    "url": "/platt-mockup/static/media/industrial.ff08a92f.png"
+  },
+  {
+    "revision": "ec691cddfe61e63ecf55f79d70d87ce5",
+    "url": "/platt-mockup/static/media/lighting.ec691cdd.png"
+  },
+  {
+    "revision": "0ca9dbc4b30895f07628e05fcbee5cd5",
+    "url": "/platt-mockup/static/media/platt-plus.0ca9dbc4.png"
+  },
+  {
+    "revision": "8b17887befef45376d5d2e996183ddc7",
+    "url": "/platt-mockup/static/media/super-tool-day.8b17887b.png"
+  },
+  {
+    "revision": "c83bc1d1cf6a072d6907d5171f747e08",
+    "url": "/platt-mockup/static/media/utility.c83bc1d1.png"
+  }
+]);
